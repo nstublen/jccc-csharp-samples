7279111cef943a0a85a65185dbd8d51b46cb11d0
Neal Stublen <neal@treefroglabs.com>
2014-01-31 01:19:11 -0600

    Added CustomerLinq and LinqSample projects.

---
+++ CustomerLinq/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace JCCC.Samples.CustomerLinq
+{
+    static class Program
+    {
+        public class Customer
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string ZipCode { get; set; }
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            List<Customer> customers = new List<Customer>();
+
+            string cxnString = "Data Source=.\\SQLEXPRESS;"
+                + "Initial Catalog=MMABooks;"
+                + "Integrated Security=True;";
+            using (SqlConnection cxn = new SqlConnection(cxnString))
+            {
+                cxn.Open();
+
+                string select = "SELECT * FROM Customers ORDER BY Name";
+                using (SqlCommand cmd = new SqlCommand(select, cxn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                    {
+                        while (reader.Read())
+                        {
+                            Customer c = new Customer();
+                            c.ID = reader.GetInt32(reader.GetOrdinal("CustomerID"));
+                            c.Name = reader.GetString(reader.GetOrdinal("Name"));
+                            c.Address = reader.GetString(reader.GetOrdinal("Address"));
+                            c.City = reader.GetString(reader.GetOrdinal("City"));
+                            c.State = reader.GetString(reader.GetOrdinal("State"));
+                            c.ZipCode = reader.GetString(reader.GetOrdinal("ZipCode"));
+
+                            customers.Add(c);
+                        }
+                    } // SqlDataReader
+                } // SqlCommand
+            } // SqlConnection
+
+            var matches = from c in customers
+                          where c.Name.StartsWith("Ce")
+                          orderby c.Name
+                          select c;
+            foreach (Customer c in matches)
+            {
+                Console.WriteLine(c.Name);
+            }
+            MessageBox.Show(String.Format("Found {0} customers", matches.Count()));
+        }
+    }
+}
---
+++ LinqSample/FormLinqSample.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JCCC.Samples.LinqSample
+{
+    public partial class FormLinqSample : Form
+    {
+        public FormLinqSample()
+        {
+            InitializeComponent();
+        }
+
+        private bool CheckName(string inName, string inSubstring)
+        {
+            string[] parts = inName.Split(',');
+            return parts[0].Trim().Contains(inSubstring);
+        }
+
+        private void FormLinqSample_Load(object sender, EventArgs e)
+        {
+            MMABooksDataContext db = new MMABooksDataContext();
+
+            var inv = from c in db.Customers
+                      join i in db.Invoices
+                      on c.CustomerID equals i.CustomerID
+                      orderby c.Name
+                      select new { c.Name, i.InvoiceDate, i.InvoiceTotal };
+            foreach (var i in inv)
+            {
+                Console.WriteLine(String.Format("{0}, {1}, {2}", i.Name, i.InvoiceDate, i.InvoiceTotal));
+            }
+
+            var customers = from c in db.Customers
+                            orderby c.Name
+                            select c;
+
+            var list = new List<Customer>();
+            foreach (Customer c in customers)
+            {
+                list.Add(c);
+            }
+
+            var reduced = from c in list
+                          where CheckName(c.Name, "ub")
+                          select c;
+
+            foreach (Customer c in reduced)
+            {
+                listBox1.Items.Add(c.Name);
+            }
+
+            foreach (Customer c in customers)
+            {
+                c.Name.Replace("ub", "--ub--");
+            }
+
+            db.SubmitChanges();
+
+            foreach (Customer c in customers)
+            {
+                c.Name.Replace("--ub--", "ub");
+            }
+
+            db.SubmitChanges();
+
+            Customer me = new Customer();
+            me.Name = "Stublen, Neal";
+            me.Address = "12345 College Ave.";
+            me.City = "Overland Park";
+            me.State = "KS";
+            me.ZipCode = "66???";
+            db.Customers.InsertOnSubmit(me);
+
+            db.SubmitChanges();
+
+            Invoice invoice = new Invoice();
+            invoice.InvoiceDate = DateTime.Now;
+            me.Invoices.Add(invoice);
+
+            db.SubmitChanges();
+        }
+    }
+}
