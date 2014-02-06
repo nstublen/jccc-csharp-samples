c15f3b22a86f2c0c5656c9c6d82f431b6783a73e
Neal Stublen <neal@treefroglabs.com>
2014-02-05 23:06:28 -0600

    Added LINQ to SQL with table join.

---
+++ LinqJoin/FormLinqJoin.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace JCCC.Samples.LinqJoin
+{
+    partial class FormLinqJoin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvwResults = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.lvwResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvwResults.Location = new System.Drawing.Point(12, 40);
+            this.lvwResults.Name = "listView1";
+            this.lvwResults.Size = new System.Drawing.Size(478, 308);
+            this.lvwResults.TabIndex = 0;
+            this.lvwResults.UseCompatibleStateImageBehavior = false;
+            this.lvwResults.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Total";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // FormLinqJoin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 360);
+            this.Controls.Add(this.lvwResults);
+            this.Name = "FormLinqJoin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormLinqJoin_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwResults;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ LinqJoin/FormLinqJoin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JCCC.Samples.LinqJoin
+{
+    public partial class FormLinqJoin : Form
+    {
+        public FormLinqJoin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLinqJoin_Load(object sender, EventArgs e)
+        {
+            CustomerInvoicesDataContext db = new CustomerInvoicesDataContext();
+            var results = from c in db.Customers
+                          orderby c.Name
+                          join i in db.Invoices on c.CustomerID equals i.CustomerID
+                          select new { Name = c.Name, InvoiceDate = i.InvoiceDate, InvoiceTotal = i.InvoiceTotal };
+
+            foreach (var result in results)
+            {
+                ListViewItem lvi = new ListViewItem(result.Name);
+                lvi.SubItems.Add(result.InvoiceDate.ToShortDateString());
+                lvi.SubItems.Add(result.InvoiceTotal.ToString("c"));
+                lvwResults.Items.Add(lvi);
+            }
+        }
+    }
+}
